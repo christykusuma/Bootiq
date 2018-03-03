acedf3fdbd2ee15912734ed8f9da791065d6ff62
christykusuma <christykusuma96@gmail.com>
2018-03-04 06:19:49 +0700

    got backend cart working

--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -11,62 +11,24 @@ module.exports = (app) => {
     app.post('/api/shoppingcart/add', (req,res) => {
 
         console.log('read me too', req.body._id);
-        // User.findById( req.body.user, (err, user) => {
-            
-        // });
-        // const product = await Product.findById({ req.body._id });
 
-        const cart = new Cart( 
-            _product: undefined,
-            quantity: 2 
-        );
+        const cart = new Cart(); 
+        cart._product = {};
+        cart.quantity = 2;
 
         cart.save()
             .then(() => User.findById( req.body.user ))
             .then((user) => {
                 user._carts.push(cart);
                 return user.save();
-            })
-            .then(() => Product.findById({ req.body._id }))
+            });
+            
+        Product.findById( req.body._id )
             .then((product) => {
-                cart._product.push(product);
+                cart._product = product;
                 return cart.save();
             });
-
-        // Promise.all([user.save(), cart.save()])
-        //     .then((error, cartSaved) => {
-        //         if(error){
-        //             res.send(error);
-        //             console.log("error saving your cart to the database");
-        //         }
-            
-        //         res.json({
-        //             message: "Successfully saved cart to database",
-        //             cartSaved: cartSaved
-        //         });
-        // })
-    });
-
-        // Finding user id and pushing product into shopping cart
-        // User.findById({ req.user.id })
-        // .then((user) => {
-        //     user.cart.push({ 
-        //         _product: req.body._id,
-        //         quantity: 2
-        //     });
-        //     return user.save(
-        //         (error, cartSaved) => {
-        //             if(error){
-        //                 res.send(error);
-        //                 console.log("error saving your cart to the database");
-        //             }
-                
-        //             res.json({
-        //                 message: "Successfully saved cart to database",
-        //                 cartSaved: cartSaved
-        //             });
-        //     });
-        // });    
+    });  
 
     // // Deleting a product from shopping cart
     // app.post('/api/shoppingcart/delete', (req,res) => {
